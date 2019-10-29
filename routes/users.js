4dce525687067f8d0b596f276dac0248df2e0767
Rinsa Fathima <rinsafathima09@gmail.com>
2019-10-30 00:34:00 +0530

    home page,login page and signup page added

--- routes/users.js
+++ routes/users.js
@@ -5,5 +5,12 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
-
+router.get('/login',function (req,res,next){
+  
+  res.render('user/login')
+})
+router.get('/signup',function (req,res,next){
+  
+  res.render('user/signup')
+})
 module.exports = router;
